96b0d75efa2bdf2f468458df63053ca332aee389
lzhijin <1094397644@qq.com>
2020-05-12 15:56:52 +0800

    单例模式

---
+++ src/com/point/designModel/singleton/LazySingleton.java
@@ -0,0 +1,23 @@
+package com.point.designModel.singleton;
+
+/**
+ * Created by lzhijin Cotter on 2020/5/12.
+ * 懒汉单例模式
+ */
+public class LazySingleton {
+
+    private static volatile LazySingleton instance = null;  // 统一从内存中获取instance,保证线程同步
+    private LazySingleton(){} // 构造方法私有化
+
+    public static LazySingleton getInstance(){
+        if (instance == null){
+            synchronized (instance){
+                if (instance == null){
+                    instance = new LazySingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+}
